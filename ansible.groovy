be5c9e0d983413cb65c2d0b11cbb832aaeadb0b3
Islam Kambarov <ikambarov@yahoo.com>
2022-11-21 15:36:49 -0600

    Use withCredentials

--- ansible.groovy
+++ ansible.groovy
@@ -1,5 +1,10 @@
 node{
     stage("Check Connection"){
-        sh "ansible --version"
+        withCredentials([sshUserPrivateKey(credentialsId: 'ansible-sshkey', keyFileVariable: 'ANSIBLE_KEY', usernameVariable: 'ANSIBLE_USER')]) {
+            sh """
+                export ANSIBLE_HOST_KEY_CHECKING=False
+                ansible -i '167.172.136.103,' --private-key=$ANSIBLE_KEY all -m ping
+            """
+        }
     }
 }
